5334906d9b3b75c40e83f07823d605164b6a93b4
Catherine Jarocki <cjarocki@gmail.com>
2021-02-19 21:15:11 -0600

    0 not None

--- main.py
+++ main.py
@@ -4,21 +4,25 @@ import threading
 moveMouseUp = True
 count = 1
 
+
 def set_interval(func, sec):
     def func_wrapper():
         set_interval(func, sec)
         func()
+
     t = threading.Timer(sec, func_wrapper)
     t.start()
     return t
+
 
 def moveMouse():
     global moveMouseUp
     global count
     pixelsToMove = 1 if moveMouseUp else -1
-    pyautogui.moveRel(None, pixelsToMove)
+    pyautogui.moveRel(0, pixelsToMove)
     moveMouseUp = not moveMouseUp
-    print ('Moved mouse ' + str(count) + ' times')
+    print('Moved mouse ' + str(count) + ' times')
     count += 1
+
 
 set_interval(moveMouse, 60)
